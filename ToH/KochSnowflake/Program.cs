ddcd3c0a08a34947e3559dcf684a935fe2ecae11
Maksim Lyubimov <sackliem@mail.ru>
2024-10-23 11:42:13 +0500

    init

---
+++ ToH/KochSnowflake/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KochSnowflake
+{
+    public class KochSnowflake : Form
+    {
+        private const int Width = 1280;
+        private const int Height = 1000;
+
+        public KochSnowflake()
+        {
+            this.Text = "Koch Snowflake";
+            this.Size = new Size(Width, Height);
+            this.Paint += new PaintEventHandler(this.DrawSnowflake);
+        }
+
+        private void DrawSnowflake(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(Color.Blue, 1);
+
+            // Исходные вершины равностороннего треугольника
+            PointF pointA = new PointF(200, 700);
+            PointF pointB = new PointF(800, 700);
+            PointF pointC = new PointF(500, 700-(float)(600 * Math.Sqrt(3) / 2)); // ахуетительное вычисление третьей вершины
+
+            // Рисуем три стороны треугольника с рекурсивными линиями Коха, применяя повороты
+            DrawKochLine(g, pen, pointA, pointB, 10); // первая сторона
+            DrawKochLine(g, pen, pointB, pointC, 10); // вторая сторона
+            DrawKochLine(g, pen, pointC, pointA, 10); // третья сторона с поворотом
+        }
+
+        private void DrawKochLine(Graphics g, Pen pen, PointF p1, PointF p2, int level)
+        {
+            if (level == 0)
+            {
+                g.DrawLine(pen, p1, p2); // Рисуем прямую линию, если достигли конца рекурсии
+            }
+            else
+            {
+                float dx = (p2.X - p1.X) / 3;
+                float dy = (p2.Y - p1.Y) / 3;
+
+                PointF pA = new PointF(p1.X + dx, p1.Y + dy);
+                PointF pB = new PointF(p1.X + 2 * dx, p1.Y + 2 * dy);
+
+                // Находим точку для "выступа" фрактала Коха
+                float midX = (p1.X + p2.X) / 2;
+                float midY = (p1.Y + p2.Y) / 2;
+
+                float length = (float)Math.Sqrt(dx * dx + dy * dy);
+                float angle = (float)Math.Atan2(p2.Y - p1.Y, p2.X - p1.X);
+
+                // Изменяем угол на +Math.PI / 3, чтобы выступ пошел в противоположную сторону
+                float newX = midX + (float)(length * Math.Cos(angle + Math.PI / 3));
+                float newY = midY + (float)(length * Math.Sin(angle + Math.PI / 3));
+
+                PointF pC = new PointF(newX, newY);
+
+                // Рекурсивно рисуем 4 части линии Коха
+                DrawKochLine(g, pen, p1, pA, level - 1);
+                DrawKochLine(g, pen, pA, pC, level - 1);
+                DrawKochLine(g, pen, pC, pB, level - 1);
+                DrawKochLine(g, pen, pB, p2, level - 1);
+            }
+        }
+
+        // Функция для поворота точки относительно другой на определённый угол
+        private PointF RotatePoint(PointF center, PointF point, float angleDegrees)
+        {
+            double angleRadians = angleDegrees * Math.PI / 180;
+            float cosTheta = (float)Math.Cos(angleRadians);
+            float sinTheta = (float)Math.Sin(angleRadians);
+
+            float dx = point.X - center.X;
+            float dy = point.Y - center.Y;
+
+            return new PointF(
+                center.X + dx * cosTheta - dy * sinTheta,
+                center.Y + dx * sinTheta + dy * cosTheta
+            );
+        }
+
+
+
+        [STAThread]
+        public static void Main()
+        {
+            Application.Run(new KochSnowflake());
+        }
+    }
+}
---
+++ ToH/Lab2/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Button = System.Windows.Forms.Button;
+using TextBox = System.Windows.Forms.TextBox;
+
+namespace Lab2
+{
+    public partial class Form1 : Form
+    {
+        private int numberOfDisks = 0; 
+        private Stack<int>[] rods; 
+        private int movesMade = 0;
+        private int movingDisk = -1; 
+        private int movingDiskX; 
+        private int movingDiskY; 
+        private Button buttonStop; 
+        private CancellationTokenSource cancellationTokenSource; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true; 
+
+            buttonStop = new Button();
+            buttonStop.Text = "Остановить";
+            buttonStop.Location = new Point(253, 500); 
+            buttonStop.Click += ButtonStop_Click;
+            this.Controls.Add(buttonStop);
+
+            rods = new Stack<int>[3];
+            for (int i = 0; i < 3; i++)
+            {
+                rods[i] = new Stack<int>();
+            }
+            for (int i = numberOfDisks; i > 0; i--)
+            {
+                rods[0].Push(i);
+            }
+
+            this.Paint += new PaintEventHandler(Form1_Paint);
+            textBox2.Text = "Диски";
+            textBox2.ForeColor = Color.Gray;
+            textBox2.GotFocus += RemoveText;
+            textBox2.LostFocus += AddText;
+        }
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawRods(e.Graphics);
+            if (movingDisk >= 0)
+            {
+                // Отрисовываем перемещаемый диск
+                e.Graphics.FillRectangle(Brushes.LightSlateGray, movingDiskX, movingDiskY, movingDisk * 20, 12);
+            }
+        }
+
+        private void DrawRods(Graphics graphics)
+        {
+            int rodWidth = 10;
+            int rodHeight = 150;
+            int baseHeight = 300;
+            int spacing = 200;
+
+            for (int i = 0; i < rods.Length; i++)
+            {
+                int x = i * spacing + 100; 
+                graphics.FillRectangle(Brushes.Black, x - rodWidth / 2, baseHeight - rodHeight, rodWidth, rodHeight);
+
+                int diskOffset = 0; 
+                foreach (var disk in rods[i])
+                {
+                    graphics.FillRectangle(Brushes.Gray, x - disk * 10, baseHeight - rodHeight + diskOffset - 10, disk * 20, 12);
+                    diskOffset += 20;
+                }
+            }
+        }
+
+        private async Task MoveDiskWithAnimation(int fromRod, int toRod)
+        {
+            if (rods[fromRod].Count > 0)
+            {
+                int disk = rods[fromRod].Pop();
+                int targetX = (toRod * 200 + 100) - disk * 10; 
+                int fromX = (fromRod * 200 + 100) - disk * 10; 
+                movingDiskX = fromX; 
+                int y = 130; 
+                movingDiskY = y;
+                
+                movingDisk = disk;
+
+                for (int i = 0; i < 20; i++)
+                {
+                    this.cancellationTokenSource.Token.ThrowIfCancellationRequested();
+                    y -= 5; 
+                    movingDiskY = y;
+                    this.Invalidate(); 
+                    await Task.Delay(30);
+                }
+
+                movingDiskX = targetX; 
+                for (int i = 0; i < 20; i++)
+                {
+                    this.cancellationTokenSource.Token.ThrowIfCancellationRequested();
+                    y += 5; 
+                    movingDiskY = y;
+                    this.Invalidate(); 
+                    await Task.Delay(30); 
+                }
+
+                rods[toRod].Push(disk);
+                movesMade++;
+                movingDisk = -1; 
+                this.Invalidate(); 
+            }
+        }
+
+        private async Task SolveHanoi(int n, int fromRod, int toRod, int tempRod)
+        {
+            if (n > 0)
+            {
+                await SolveHanoi(n - 1, fromRod, tempRod, toRod);
+                await MoveDiskWithAnimation(fromRod, toRod);
+                await SolveHanoi(n - 1, tempRod, toRod, fromRod);
+            }
+        }
+
+        private async void buttonMove_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox2.Text, out int newNumberOfDisks) && newNumberOfDisks > 0)
+            {
+                numberOfDisks = newNumberOfDisks;
+
+                rods = new Stack<int>[3];
+                for (int i = 0; i < 3; i++)
+                {
+                    rods[i] = new Stack<int>();
+                }
+                for (int i = numberOfDisks; i > 0; i--)
+                {
+                    rods[0].Push(i);
+                }
+
+                movesMade = 0; 
+                cancellationTokenSource = new CancellationTokenSource();
+                await SolveHanoi(numberOfDisks, 0, 2, 1); 
+                MessageBox.Show($"Сделано движений: {movesMade}");
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите натуральное число дисков."); 
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonStop_Click(object sender, EventArgs e)
+        {
+            cancellationTokenSource?.Cancel(); 
+        }
+
+        private void RemoveText(object sender, EventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox.Text == "количество дисков")
+            {
+                textBox.Text = "";
+                textBox.ForeColor = Color.Black;
+            }
+        }
+
+        private void AddText(object sender, EventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "количество дисков";
+                textBox.ForeColor = Color.Gray;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            buttonMove_Click(sender, e);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cancellationTokenSource?.Cancel();
+        }
+    }
+}
